0a730882b90285b43b812fe90c0733264619d225
Claziero <lorenzoclazzer.2000@outlook.it>
2022-07-03 19:33:30 +0200

    Added dataset splitting function

--- OCR/datasetGenerator.py
+++ OCR/datasetGenerator.py
@@ -2,6 +2,7 @@
 import os
 from PIL import Image
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
 
 # Define colors
@@ -12,7 +13,7 @@ TEXT_RESET = '\033[0m'
 images_path = '../LicensePlateGenerator/output/'
 
 # Function to convert a number plate string in 134 ints
-def convert_to_ints(string) -> list[int]:
+def convert_to_ints(string:str) -> list[int]:
     # Convert the string to a list of ints
     ints = []
 
@@ -41,7 +42,7 @@ def convert_to_ints(string) -> list[int]:
     return ints
 
 # Function to generate the dataset in .npy format
-def generate_dataset_binary(path, output_path) -> None:
+def generate_dataset_binary(path:str, output_path:str) -> None:
     # Get all the images in the path
     images = os.listdir(path)
     # Create a new folder for the dataset
@@ -58,7 +59,7 @@ def generate_dataset_binary(path, output_path) -> None:
     return
 
 # Function to generate the dataset in .csv format
-def generate_dataset_csv(path) -> None:
+def generate_dataset_csv(path:str) -> None:
     # Create a new output file "dataset.csv"
     dataset = open('dataset.csv', 'w+')
 
@@ -79,10 +80,34 @@ def generate_dataset_csv(path) -> None:
     dataset.close()
     return
 
+# Function to split the dataset
+def split_dataset(path:str, train:str, test:str, perc:int=80) -> None:
+    dataset = pd.read_csv(path, header=None)
+    train_d = open(train, 'w+')
+    test_d = open(test, 'w+')
+
+    # Split the dataset in train and test
+    train_dataset = dataset.sample(frac=perc/100, random_state=42)
+    test_dataset = dataset.drop(train_dataset.index)
+
+    # Write the datasets
+    train_dataset.to_csv(train_d, index=False, header=False, line_terminator='\n')
+    test_dataset.to_csv(test_d, index=False, header=False, line_terminator='\n')
+
+    train_d.close()
+    test_d.close()
+    return
+    
 
 if __name__ == '__main__':
     print(TEXT_GREEN 
         + '>> Generating dataset from {} ...'.format(images_path)
         + TEXT_RESET)
     generate_dataset_csv(images_path)
-    # generate_dataset_binary(images_path, 'dataset/')
+    
+    print(TEXT_GREEN 
+        + '>> Splitting dataset from {} to {} ({}%), {} ({}%) ...'.format('dataset.csv',
+        'dataset_train.csv', 80, 'dataset_test.csv', 20)
+        + TEXT_RESET)
+    split_dataset('dataset.csv', 'dataset_train.csv', 'dataset_test.csv')
+    print(TEXT_GREEN + '>> Done.' + TEXT_RESET)
