de4a491db551126d5efb03fa701a896a1d80db7f
Claziero <lorenzoclazzer.2000@outlook.it>
2022-07-05 16:38:59 +0200

    Added driver for dataset generator

--- OCR/datasetGenerator.py
+++ OCR/datasetGenerator.py
@@ -6,11 +6,9 @@ import pandas as pd
 from tqdm import tqdm
 
 # Define colors
-TEXT_GREEN = '\033[92m'
 TEXT_RESET = '\033[0m'
-
-# Input path containing images
-images_path = '../LicensePlateGenerator/output/'
+TEXT_GREEN = '\033[92m'
+TEXT_YELLOW = '\033[93m'
 
 # Function to convert a number plate string in 134 ints
 def convert_to_ints(string:str) -> list[int]:
@@ -41,27 +39,10 @@ def convert_to_ints(string:str) -> list[int]:
 
     return ints
 
-# Function to generate the dataset in .npy format
-def generate_dataset_binary(path:str, output_path:str) -> None:
-    # Get all the images in the path
-    images = os.listdir(path)
-    # Create a new folder for the dataset
-    os.makedirs(output_path)
-    # For each image in the path
-    for image in tqdm(images):
-        if image.endswith('.png'):
-            # Open the image
-            img = Image.open(path + image)
-            # Convert the image to numpy array
-            img = np.array(img)
-            # Save the image in the new folder
-            np.save(output_path + "/" + image, img)
-    return
-
 # Function to generate the dataset in .csv format
-def generate_dataset_csv(path:str) -> None:
+def generate_dataset_csv(path:str, filename:str) -> None:
     # Create a new output file "dataset.csv"
-    dataset = open('dataset.csv', 'w+')
+    dataset = open(filename, 'w+')
 
     # Get all the images in the path
     images = os.listdir(path)
@@ -99,15 +80,107 @@ def split_dataset(path:str, train:str, test:str, perc:int=80) -> None:
     return
     
 
+# Main function
+def driver_main():
+    choice = 1
+
+    while choice != '0':
+        # Get the user input
+        print(TEXT_YELLOW + '>> Driver helper. Select the function to run. Type:' + TEXT_RESET)
+        print('  1. Generate all datasets (full + train + test).')
+        print('  2. Generate full dataset only.')
+        print('  3. Split full dataset into train and test datasets.')
+        print('  0. Exit.')
+        choice = input(TEXT_YELLOW + 'Enter your choice: ' + TEXT_RESET)
+
+        # Generate all datasets
+        if choice == '1':
+            images_path = input('Enter the file path containing the images [Enter = \"../LicensePlateGenerator/output/\"]: ')
+            if images_path == '':
+                images_path = '../LicensePlateGenerator/output/'
+
+            filename = input('Enter the filename of the dataset [Enter = \"dataset.csv\"]: ')
+            if filename == '':
+                filename = 'dataset.csv'
+
+            filename_train = input('Enter the filename of the train dataset [Enter = \"dataset_train.csv\"]: ')
+            if filename_train == '':
+                filename_train = 'dataset_train.csv'
+
+            filename_test = input('Enter the filename of the test dataset [Enter = \"dataset_test.csv\"]: ')
+            if filename_test == '':
+                filename_test = 'dataset_test.csv'
+
+            perc = input('Enter the percentage of the dataset to use for the train dataset [Enter = \"80%\"]: ')
+            if perc == '':
+                perc = 80
+            perc = int(perc)
+
+            print(TEXT_GREEN 
+                + '>> Generating dataset from {} into {} ...'.format(images_path, filename)
+                + TEXT_RESET)      
+            generate_dataset_csv(images_path, filename)
+            
+            print(TEXT_GREEN 
+                + '>> Splitting dataset from {} to {} ({}%), {} ({}%) ...'.format(filename,
+                filename_train, perc, filename_test, 100 - perc)
+                + TEXT_RESET)
+            split_dataset(filename, filename_train, filename_test, perc)
+            print(TEXT_GREEN + '>> Done.' + TEXT_RESET)
+
+        # Generate full dataset only
+        elif choice == '2':
+            images_path = input('Enter the file path containing the images [Enter = \"../LicensePlateGenerator/output/\"]: ')
+            if images_path == '':
+                images_path = '../LicensePlateGenerator/output/'
+
+            filename = input('Enter the filename of the dataset [Enter = \"dataset.csv\"]: ')
+            if filename == '':
+                filename = 'dataset.csv'
+
+            print(TEXT_GREEN 
+                + '>> Generating dataset from {} into {} ...'.format(images_path, filename)
+                + TEXT_RESET)      
+            generate_dataset_csv(images_path, filename)
+            print(TEXT_GREEN + '>> Done.' + TEXT_RESET)
+
+        # Split full dataset into train and test datasets
+        elif choice == '3':
+            filename = input('Enter the filename of the dataset [Enter = \"dataset.csv\"]: ')
+            if filename == '':
+                filename = 'dataset.csv'
+
+            filename_train = input('Enter the filename of the train dataset [Enter = \"dataset_train.csv\"]: ')
+            if filename_train == '':
+                filename_train = 'dataset_train.csv'
+
+            filename_test = input('Enter the filename of the test dataset [Enter = \"dataset_test.csv\"]: ')
+            if filename_test == '':
+                filename_test = 'dataset_test.csv'
+
+            perc = input('Enter the percentage of the dataset to use for the train dataset [Enter = \"80%\"]: ')
+            if perc == '':
+                perc = 80
+            perc = int(perc)
+
+            print(TEXT_GREEN 
+                + '>> Splitting dataset from {} to {} ({}%), {} ({}%) ...'.format(filename,
+                filename_train, perc, filename_test, 100 - perc)
+                + TEXT_RESET)
+            split_dataset(filename, filename_train, filename_test, perc)
+            print(TEXT_GREEN + '>> Done.' + TEXT_RESET)
+
+        # Exit
+        elif choice == '0':
+            print(TEXT_YELLOW + '>> Exiting.' + TEXT_RESET)
+            break
+
+        # Invalid input
+        else:
+            print(TEXT_YELLOW + '>> Invalid choice.' + TEXT_RESET)
+            continue
+
+    return
+
 if __name__ == '__main__':
-    print(TEXT_GREEN 
-        + '>> Generating dataset from {} ...'.format(images_path)
-        + TEXT_RESET)
-    generate_dataset_csv(images_path)
-    
-    print(TEXT_GREEN 
-        + '>> Splitting dataset from {} to {} ({}%), {} ({}%) ...'.format('dataset.csv',
-        'dataset_train.csv', 80, 'dataset_test.csv', 20)
-        + TEXT_RESET)
-    split_dataset('dataset.csv', 'dataset_train.csv', 'dataset_test.csv')
-    print(TEXT_GREEN + '>> Done.' + TEXT_RESET)
+    driver_main()
